2838df5f1583c036b0ad4625421dd028f6d58921
LuZhangTina <zhanglu.xidian@gmail.com>
2019-09-21 12:56:28 +1000

    add test case: find product by exist productId

--- src/main/java/com/tina/hashina/tinaausbuy/module/Product.java
+++ src/main/java/com/tina/hashina/tinaausbuy/module/Product.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name="product")
@@ -68,6 +69,25 @@ public class Product implements Serializable {
 
     @Override
     public String toString() {
-        return "Product Id: " + getProductId().toString() + " EngName: " + getProNameEng() + " Price: " + getPriceRmb().toString();
+        return "Product Id: " + getProductId().toString()
+                + " EngName: " + getProNameEng()
+                + " Price: " + getPriceRmb().getAmount().toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.measureValue, measureValue) == 0 &&
+                productId.equals(product.productId) &&
+                proNameEng.equals(product.proNameEng) &&
+                Objects.equals(proNameChn, product.proNameChn) &&
+                measureUnit == product.measureUnit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, proNameEng, proNameChn, measureValue, measureUnit);
     }
 }
--- src/test/java/com/tina/hashina/tinaausbuy/IntegrationTest.java
+++ src/test/java/com/tina/hashina/tinaausbuy/IntegrationTest.java
@@ -1,6 +1,10 @@
 package com.tina.hashina.tinaausbuy;
 
+import com.tina.hashina.tinaausbuy.module.MeasureUnit;
 import com.tina.hashina.tinaausbuy.module.Product;
+import com.tina.hashina.tinaausbuy.service.ProductService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +14,56 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
-
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    private ProductService productService;
+
+    private List<Product> products = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        Product product = productService.createProduct("Test01",
+                "测试产品01", 5,25, 25,
+                MeasureUnit.GRAM);
+        products.add(product);
+
+        product = productService.createProduct("Test02",
+                "测试产品02", 15,75, 100,
+                MeasureUnit.MILLILITER);
+        products.add(product);
+    }
 
     @Test
     public void getProductByProductId_shouldReturnNotFoundForNotExistProductId() {
-        ResponseEntity<Product> responseEntity = testRestTemplate.getForEntity("/products/{productId}", Product.class, "1");
+        ResponseEntity<Product> responseEntity = testRestTemplate
+                .getForEntity("/products/{productId}", Product.class, "3");
         assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void getProductByProductId_shouldReturnProductForExistProductId() {
+        Product product = products.get(1);
+        ResponseEntity<Product> responseEntity = testRestTemplate
+                .getForEntity("/products/{productId}", Product.class, product.getProductId());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(product, responseEntity.getBody());
+    }
+
+    @After
+    public void rollback() {
+        products.forEach(product -> {
+            productService.deleteProduct(product.getProductId());
+        });
+
+        products.clear();
     }
 }
